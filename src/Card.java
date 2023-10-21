6f187671cb7862f4c8b1cc54ea44c91cdfe584fb
Rayatk <rayatkibria1999@gmail.com>
2023-10-21 01:49:09 -0400

    Updated card

--- src/Card.java
+++ src/Card.java
@@ -1,26 +1,36 @@
 package src;
+
 public class Card {
 
-    enum Color {
-        RED, GREEN, BLUE, YELLOW, WILD
-    }
-    enum Value {
-        ZERO, ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, SKIP, REVERSE, DRAW_TWO, WILD
-    }
-    private final Color color;
-    private final Value value;
+    public enum Color {RED, GREEN, BLUE, YELLOW, WILD}
+    public enum Type {ZERO, ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, SKIP, REVERSE, DRAW_ONE, WILD_DRAW_TWO}
+    private Color color;
+    private Type type;
+    private boolean used = false;
 
-    public Card(Color color, Value value) {
+    public Card(Color color, Type type) {
         this.color = color;
-        this.value = value;
+        this.type = type;
     }
+
     public Color getColor() {
-        return color;
+        return this.color;
     }
-    public Value getValue() {
-        return value;
+
+    public Type getType() {
+        return this.type;
     }
+
+    public boolean getUsed() { return this.used; }
+
+    public void setColor(Color color) { this.color = color; }
+
+    public void setType(Type type) { this.type = type; }
+
+    public void setUsed() { this.used = true; }
+
+    @Override
     public String toString() {
-        return color + " " + value;
+        return color + " " + type;
     }
 }
