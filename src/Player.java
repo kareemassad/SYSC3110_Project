36eed809fc4a6c7774ed8f2284e5074fbe624639
Rayatk <rayatkibria1999@gmail.com>
2023-10-21 04:47:20 -0400

    Updated card

--- src/Player.java
+++ src/Player.java
@@ -4,26 +4,65 @@ import java.util.List;
 
 public class Player {
     private String name;
-    private List<Card> playerHand;
+    private List<Card> cards;
+    private int score;
 
     public Player(String name) {
         this.name = name;
-        playerHand = new ArrayList<>();
+        this.cards = new ArrayList<>();
+        this.score = 0;
     }
 
     public String getName() {
-        return name;
+        return this.name;
     }
 
-    public List<Card> getPlayerHand() {
-        return playerHand;
+    public void setName(String name) { this.name = name; }
+
+    public int getScore() { return score; }
+
+    public void updateScore(Card card) {
+        if (card.getType() == Card.Type.ZERO || card.getType() == Card.Type.ONE || card.getType() == Card.Type.TWO || card.getType() == Card.Type.THREE || card.getType() == Card.Type.FOUR || card.getType() == Card.Type.FIVE || card.getType() == Card.Type.SIX || card.getType() == Card.Type.SEVEN || card.getType() == Card.Type.EIGHT || card.getType() == Card.Type.NINE) {
+            this.score += card.getType().ordinal();
+        } else if (card.getType() == Card.Type.DRAW_ONE) {
+            this.score += 10;
+        } else if (card.getType() == Card.Type.REVERSE || card.getType() == Card.Type.SKIP) {
+            this.score += 20;
+        } else if (card.getType() == Card.Type.WILD) {
+            this.score += 40;
+        } else if (card.getType() == Card.Type.WILD_DRAW_TWO) {
+            this.score += 50;
+        }
     }
 
-    public void drawCard(Card c) {
-        playerHand.add(c);
+    public Card getCard(int index) {
+        return this.cards.get(index);
     }
 
-    public void playCard(Card c) {
-        playerHand.remove(c);
+    public void addCards(Card c) {
+        this.cards.add(c);
+    }
+
+    public void removeCards(int i) {
+        this.cards.remove(i);
+    }
+
+    public int getSize() {return this.cards.size(); }
+
+    @Override
+    public String toString() {
+        StringBuilder s = new StringBuilder("Your Score: " + getScore() + "\nYour cards:\n");
+        int count = 1;
+
+        for (Card c : cards) {
+            if (count == cards.size()) {
+                s.append(count).append(". ").append(c);
+            } else {
+                s.append(count).append(". ").append(c).append("\n");
+                count++;
+            }
+        }
+
+        return s.toString();
     }
 }
