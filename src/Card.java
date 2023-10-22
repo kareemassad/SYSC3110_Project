360891054b6c8dfc7563bea0f5f3c9e6fcc7db0f
Mehedi Mostofa <91309756+mehedimostofa@users.noreply.github.com>
2023-10-22 13:25:02 -0400

    Wrote Java Docs for Card.java

--- src/Card.java
+++ src/Card.java
@@ -1,5 +1,8 @@
 package src;
 
+/**
+ * This is the Card Class. Card class represents the number and special cards along with their colors using Enumeration.
+ */
 public class Card {
 
     public enum Color {RED, GREEN, BLUE, YELLOW, WILD}
@@ -8,27 +11,52 @@ public class Card {
     private Type type;
     private boolean used = false;
 
+    /**
+     * Default constructor for Card
+     */
     public Card(Color color, Type type) {
         this.color = color;
         this.type = type;
     }
 
+    /**
+     * Default getter for card's color.
+     */
     public Color getColor() {
         return color;
     }
 
+    /**
+     * Default getter for card's type.
+     */
     public Type getType() {
         return type;
     }
 
+    /**
+     * It checks if a declared card is used or no.
+     * @return
+     */
     public boolean getUsed() { return used; }
 
+    /**
+     * Default setter for card's color.
+     */
     public void setColor(Color color) { this.color = color; }
 
+    /**
+     *Default setter for card's type.
+     */
     public void setType(Type type) { this.type = type; }
 
+    /**
+     * It declares if a card is used or not.
+     */
     public void setUsed() { this.used = true; }
 
+    /**
+     * toString method to represent the card in readable text format.
+     */
     @Override
     public String toString() {
         return color + " " + type;
